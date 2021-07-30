da72fbd96b77d3eafdc4f4b738c1b806ea8c01ab
David Vasek <david.vasek51@gmail.com>
2021-07-30 10:46:41 -0500

    react using context

---
+++ src/components/Cat.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import StateContext from "../context";
+
+const Cat = () => {
+  const [value, dispatch] = useContext(StateContext);
+  const { activity } = value;
+
+  const _handleClick = (type) => {
+    dispatch({ type });
+  };
+
+  return (
+    <>
+      <p>The Cat is {activity}.</p>
+      <button type="button" onClick={() => _handleClick("ACTION_EAT")}>
+        Eat
+      </button>
+      <button type="button" onClick={() => _handleClick("ACTION_NAP")}>
+        Nap
+      </button>
+      
+    </>
+  );
+};
+
+export default Cat;
